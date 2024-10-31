190cb88646341c93381205d57fd82f364e076729
markydso <markosdeieso@gmail.com>
2024-10-31 04:50:57 -0400

    condensed key button function into a class to remove redundant code

--- src/script.js
+++ src/script.js
@@ -144,174 +144,23 @@ function finishGame(){
   console.log(copyable_result);
 }
 
-//KEYBOARD ROW 1
-let q_key = document.getElementById("q-key");
-q_key.addEventListener('click', () => {
-    const event = new KeyboardEvent('keyup', {'key': 'q'});
-    document.dispatchEvent(event);
-});
+class KeyButton {
+  constructor(key_name){
+    this.key_name = key_name;
+    
+    const element = document.getElementById(this.key_name + "-key");
+    element.addEventListener('click', () => {
+      const event = new KeyboardEvent('keyup', {'key': this.key_name});
+      document.dispatchEvent(event);
+    });
+  }
+  
+}
 
-let w_key = document.getElementById("w-key");
-w_key.addEventListener('click', () => {
-    const event = new KeyboardEvent('keyup', {'key': 'w'});
-    document.dispatchEvent(event);
-});
-
-let e_key = document.getElementById("e-key");
-e_key.addEventListener('click', () => {
-    const event = new KeyboardEvent('keyup', {'key': 'e'});
-    document.dispatchEvent(event);
-});
-
-let r_key = document.getElementById("r-key");
-r_key.addEventListener('click', () => {
-    const event = new KeyboardEvent('keyup', {'key': 'r'});
-    document.dispatchEvent(event);
-});
-
-let t_key = document.getElementById("t-key");
-t_key.addEventListener('click', () => {
-    const event = new KeyboardEvent('keyup', {'key': 't'});
-    document.dispatchEvent(event);
-});
-
-let y_key = document.getElementById("y-key");
-y_key.addEventListener('click', () => {
-    const event = new KeyboardEvent('keyup', {'key': 'y'});
-    document.dispatchEvent(event);
-});
-
-let u_key = document.getElementById("u-key");
-u_key.addEventListener('click', () => {
-    const event = new KeyboardEvent('keyup', {'key': 'u'});
-    document.dispatchEvent(event);
-});
-
-let i_key = document.getElementById("i-key");
-i_key.addEventListener('click', () => {
-    const event = new KeyboardEvent('keyup', {'key': 'i'});
-    document.dispatchEvent(event);
-});
-
-let o_key = document.getElementById("o-key");
-o_key.addEventListener('click', () => {
-    const event = new KeyboardEvent('keyup', {'key': 'o'});
-    document.dispatchEvent(event);
-});
-
-let p_key = document.getElementById("p-key");
-p_key.addEventListener('click', () => {
-    const event = new KeyboardEvent('keyup', {'key': 'p'});
-    document.dispatchEvent(event);
-});
-
-//KEYBOARD ROW 2
-let a_key = document.getElementById("a-key");
-a_key.addEventListener('click', () => {
-    const event = new KeyboardEvent('keyup', {'key': 'a'});
-    document.dispatchEvent(event);
-});
-
-let s_key = document.getElementById("s-key");
-s_key.addEventListener('click', () => {
-    const event = new KeyboardEvent('keyup', {'key': 's'});
-    document.dispatchEvent(event);
-});
-
-let d_key = document.getElementById("d-key");
-d_key.addEventListener('click', () => {
-    const event = new KeyboardEvent('keyup', {'key': 'd'});
-    document.dispatchEvent(event);
-});
-
-let f_key = document.getElementById("f-key");
-f_key.addEventListener('click', () => {
-    const event = new KeyboardEvent('keyup', {'key': 'f'});
-    document.dispatchEvent(event);
-});
-
-let g_key = document.getElementById("g-key");
-g_key.addEventListener('click', () => {
-    const event = new KeyboardEvent('keyup', {'key': 'g'});
-    document.dispatchEvent(event);
-});
-
-let h_key = document.getElementById("h-key");
-h_key.addEventListener('click', () => {
-    const event = new KeyboardEvent('keyup', {'key': 'h'});
-    document.dispatchEvent(event);
-});
-
-let j_key = document.getElementById("j-key");
-j_key.addEventListener('click', () => {
-    const event = new KeyboardEvent('keyup', {'key': 'j'});
-    document.dispatchEvent(event);
-});
-
-let k_key = document.getElementById("k-key");
-k_key.addEventListener('click', () => {
-    const event = new KeyboardEvent('keyup', {'key': 'k'});
-    document.dispatchEvent(event);
-});
-
-let l_key = document.getElementById("l-key");
-l_key.addEventListener('click', () => {
-    const event = new KeyboardEvent('keyup', {'key': 'l'});
-    document.dispatchEvent(event);
-});
-
-//KEYBOARD ROW 3
-let enter_key = document.getElementById("enter-key");
-enter_key.addEventListener('click', () => {
-    const event = new KeyboardEvent('keyup', {'key': 'Enter'});
-    document.dispatchEvent(event);
-});
-
-let z_key = document.getElementById("z-key");
-z_key.addEventListener('click', () => {
-    const event = new KeyboardEvent('keyup', {'key': 'z'});
-    document.dispatchEvent(event);
-});
-
-let x_key = document.getElementById("x-key");
-x_key.addEventListener('click', () => {
-    const event = new KeyboardEvent('keyup', {'key': 'x'});
-    document.dispatchEvent(event);
-});
-
-let c_key = document.getElementById("c-key");
-c_key.addEventListener('click', () => {
-    const event = new KeyboardEvent('keyup', {'key': 'c'});
-    document.dispatchEvent(event);
-});
-
-let v_key = document.getElementById("v-key");
-v_key.addEventListener('click', () => {
-    const event = new KeyboardEvent('keyup', {'key': 'v'});
-    document.dispatchEvent(event);
-});
-
-let b_key = document.getElementById("b-key");
-b_key.addEventListener('click', () => {
-    const event = new KeyboardEvent('keyup', {'key': 'b'});
-    document.dispatchEvent(event);
-});
-
-let n_key = document.getElementById("n-key");
-n_key.addEventListener('click', () => {
-    const event = new KeyboardEvent('keyup', {'key': 'n'});
-    document.dispatchEvent(event);
-});
-
-let m_key = document.getElementById("m-key");
-m_key.addEventListener('click', () => {
-    const event = new KeyboardEvent('keyup', {'key': 'm'});
-    document.dispatchEvent(event);
-});
-
-let back_key = document.getElementById("back-key");
-back_key.addEventListener('click', () => {
-    const event = new KeyboardEvent('keyup', {'key': 'Backspace'});
-    document.dispatchEvent(event);
-});
-
+let key_button_arr = []
+for (let i = 97; i <= 122; i++) {
+  let letter = String.fromCharCode(i);
+  const key_button = new KeyButton(letter);
+}
+const enter_button = new KeyButton("Enter");
+const backspace_button = new KeyButton("Backspace");
